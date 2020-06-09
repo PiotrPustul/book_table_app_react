f4e28b3868261f123d8fc6cabc653224c36594f9
pepe199111 <piotrpustul1@gmail.com>
2020-06-09 14:12:40 +0100

    Updates

---
+++ precache-manifest.963785c581a0650e2f934319a76a1b81.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce0110ddd22afdd82f4e2c95a1b884d",
+    "url": "/book_table_app_react/index.html"
+  },
+  {
+    "revision": "a1c708b6a53edded0952",
+    "url": "/book_table_app_react/static/css/2.6c636ea6.chunk.css"
+  },
+  {
+    "revision": "d7dafc334d2f2c19178b",
+    "url": "/book_table_app_react/static/css/main.f17f03aa.chunk.css"
+  },
+  {
+    "revision": "a1c708b6a53edded0952",
+    "url": "/book_table_app_react/static/js/2.4f693bd2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/book_table_app_react/static/js/2.4f693bd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7dafc334d2f2c19178b",
+    "url": "/book_table_app_react/static/js/main.e8b1dc6e.chunk.js"
+  },
+  {
+    "revision": "12103fd1138b37929d93",
+    "url": "/book_table_app_react/static/js/runtime-main.61449225.js"
+  },
+  {
+    "revision": "b3aae960c197186cce0b0499fc960544",
+    "url": "/book_table_app_react/static/media/img1.b3aae960.jpg"
+  },
+  {
+    "revision": "2f5c9fc749f409e47b885aa661e4f1a4",
+    "url": "/book_table_app_react/static/media/img2.2f5c9fc7.jpg"
+  },
+  {
+    "revision": "52b88a535d0ef2df4bcd27c5359698b0",
+    "url": "/book_table_app_react/static/media/img3.52b88a53.jpg"
+  },
+  {
+    "revision": "122d88bb6a6ecf1b20b98252faa4561d",
+    "url": "/book_table_app_react/static/media/img4.122d88bb.jpg"
+  },
+  {
+    "revision": "9ee25884b402c495e0c03b8a699253fc",
+    "url": "/book_table_app_react/static/media/img5.9ee25884.jpg"
+  },
+  {
+    "revision": "e7bcd61bc9825a48a8d519d8088757f1",
+    "url": "/book_table_app_react/static/media/logo.e7bcd61b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/book_table_app/precache-manifest.d7c03d0862a9b61f95fe02601347d1a9.js"
+  "/book_table_app_react/precache-manifest.963785c581a0650e2f934319a76a1b81.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/book_table_app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/book_table_app_react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
