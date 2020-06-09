56048a2e633ae7d749b1b2becc7082f9cec0f782
pepe199111 <piotrpustul1@gmail.com>
2020-06-09 13:25:39 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Book_Table_App_React/book_table_app/precache-manifest.d2995fc27fb4426ffc7705141ceeed98.js"
+  "/Book_Table_App_React/precache-manifest.ba486810438490201e91da6fe98a67db.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Book_Table_App_React/book_table_app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Book_Table_App_React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
